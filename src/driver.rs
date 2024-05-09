61ef83c09588a2b73fa6a86906a364f3143afe62
Philpax <me@philpax.me>
2024-05-09 16:47:27 +0200

    feat(tl): allow specifying out dir for driver

--- src/driver.rs
+++ src/driver.rs
@@ -1,22 +1,23 @@
+use std::path::Path;
+
 use crate::semantic_analysis::semantic_state::SemanticState;
 
-pub fn build() -> anyhow::Result<()> {
+pub fn build(out_dir: Option<&Path>) -> anyhow::Result<()> {
+    println!("cargo:rerun-if-changed=types");
+
+    let cargo_out_dir = std::env::var("OUT_DIR")?;
+    let out_dir = out_dir.unwrap_or(std::path::Path::new(&cargo_out_dir));
+
     let pointer_size = std::env::var("CARGO_CFG_TARGET_POINTER_WIDTH")?.parse::<usize>()? / 8;
     let mut semantic_state = SemanticState::new(pointer_size);
 
-    println!("cargo:rerun-if-changed=types");
     for path in glob::glob("types/**/*.rstl")?.filter_map(Result::ok) {
         semantic_state.add_file(std::path::Path::new("types"), &path)?;
     }
 
     let resolved_semantic_state = semantic_state.build()?;
     for (key, module) in resolved_semantic_state.modules() {
-        super::backends::rust::write_module(
-            std::path::Path::new(&std::env::var("OUT_DIR")?),
-            key,
-            &resolved_semantic_state,
-            module,
-        )?;
+        super::backends::rust::write_module(out_dir, key, &resolved_semantic_state, module)?;
     }
 
     Ok(())
