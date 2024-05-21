6ac8985253171d73aea61da52950e108e2ba48a4
Philpax <me@philpax.me>
2024-05-21 23:58:58 +0200

    fix #7 - mege driver and main

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,33 @@
+use std::path::Path;
+
 pub mod backends;
-pub mod driver;
 pub mod grammar;
 pub mod parser;
 pub mod semantic_analysis;
+
+pub fn build(in_dir: &Path, out_dir: &Path, pointer_size: usize) -> anyhow::Result<()> {
+    std::fs::create_dir(&out_dir)?;
+
+    let mut semantic_state = semantic_analysis::SemanticState::new(pointer_size);
+
+    for path in glob::glob(&format!("{}/**/*.pyx", in_dir.display()))?.filter_map(Result::ok) {
+        semantic_state.add_file(Path::new(&in_dir), &path)?;
+    }
+
+    let resolved_semantic_state = semantic_state.build()?;
+    for (key, module) in resolved_semantic_state.modules() {
+        backends::rust::write_module(Path::new(&out_dir), key, &resolved_semantic_state, module)?;
+    }
+
+    Ok(())
+}
+
+pub fn build_script(out_dir: Option<&Path>) -> anyhow::Result<()> {
+    println!("cargo:rerun-if-changed=types");
+
+    let cargo_out_dir = std::env::var("OUT_DIR")?;
+    let out_dir = out_dir.unwrap_or(Path::new(&cargo_out_dir));
+    let pointer_size = std::env::var("CARGO_CFG_TARGET_POINTER_WIDTH")?.parse::<usize>()? / 8;
+
+    build(Path::new("types"), out_dir, pointer_size)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,19 @@
 //! to test what the output will look like without having to look at it
 //! in situ
 
-use std::path::Path;
+use std::path::PathBuf;
 
 use anyhow::Context;
-
-use pyxis::{backends, semantic_analysis::SemanticState};
 
 fn main() -> anyhow::Result<()> {
     let mut args = std::env::args();
     args.next(); // skip the executable name
-    let in_dir = args.next().context("No input directory provided")?;
-    let out_dir = args.next().unwrap_or_else(|| String::from("out"));
+    let in_dir: PathBuf = args.next().context("No input directory provided")?.into();
+    let out_dir: PathBuf = args.next().unwrap_or_else(|| String::from("out")).into();
 
     let _ = std::fs::remove_dir_all(&out_dir);
-    std::fs::create_dir(&out_dir)?;
 
     // assume hardcoded pointer size for now
     let pointer_size = 4;
-    let mut semantic_state = SemanticState::new(pointer_size);
-
-    for path in glob::glob(&format!("{in_dir}/**/*.pyx"))?.filter_map(Result::ok) {
-        semantic_state.add_file(Path::new(&in_dir), &path)?;
-    }
-
-    let resolved_semantic_state = semantic_state.build()?;
-    for (key, module) in resolved_semantic_state.modules() {
-        backends::rust::write_module(Path::new(&out_dir), key, &resolved_semantic_state, module)?;
-    }
-
-    Ok(())
+    pyxis::build(&in_dir, &out_dir, pointer_size)
 }
