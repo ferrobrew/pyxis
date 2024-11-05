324c949e405d7ef72a8192e0f2202af2cf797c50
Philpax <me@philpax.me>
2024-11-05 23:42:54 +0100

    feat(test.sh): add codegen_tests

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-PYXIS_TEST_POINTER_SIZE=4 cargo test "$@" && PYXIS_TEST_POINTER_SIZE=8 cargo test "$@"
-
+PYXIS_TEST_POINTER_SIZE=4 cargo test "$@" && PYXIS_TEST_POINTER_SIZE=8 cargo test "$@" && cargo run --example codegen_tests
