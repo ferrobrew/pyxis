63e1605895978703f4a2d0019ad4d9a7cb9895a7
Philpax <me@philpax.me>
2025-11-08 16:13:30 +0100

    feat: start laying plumbing for multiple backends

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,19 +1,34 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser)]
 struct Args {
     /// The directory containing the Pyxis source files
     in_dir: PathBuf,
+    /// The backend to use
+    #[clap(short, long)]
+    backend: Backend,
     #[clap(default_value = "out")]
     /// The directory to write the generated Rust files to
     out_dir: PathBuf,
+}
+
+#[derive(Copy, Clone, ValueEnum)]
+enum Backend {
+    Rust,
+}
+impl From<Backend> for pyxis::Backend {
+    fn from(backend: Backend) -> Self {
+        match backend {
+            Backend::Rust => pyxis::Backend::Rust,
+        }
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     std::fs::create_dir_all(&args.out_dir)?;
-    pyxis::build(&args.in_dir, &args.out_dir)
+    pyxis::build(&args.in_dir, &args.out_dir, args.backend.into())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,18 +10,27 @@ pub mod semantic;
 
 pub(crate) mod util;
 
-pub fn build(in_dir: &Path, out_dir: &Path) -> anyhow::Result<()> {
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Backend {
+    Rust,
+}
+
+pub fn build(in_dir: &Path, out_dir: &Path, backend: Backend) -> anyhow::Result<()> {
     let config = config::Config::load(&in_dir.join("pyxis.toml"))
         .context("Failed to load config for project")?;
     let mut semantic_state = semantic::SemanticState::new(config.project.pointer_size);
 
     for path in glob::glob(&format!("{}/**/*.pyxis", in_dir.display()))?.filter_map(Result::ok) {
-        semantic_state.add_file(Path::new(&in_dir), &path)?;
+        semantic_state.add_file(in_dir, &path)?;
     }
 
     let resolved_semantic_state = semantic_state.build()?;
-    for (key, module) in resolved_semantic_state.modules() {
-        backends::rust::write_module(Path::new(&out_dir), key, &resolved_semantic_state, module)?;
+    match backend {
+        Backend::Rust => {
+            for (key, module) in resolved_semantic_state.modules() {
+                backends::rust::write_module(out_dir, key, &resolved_semantic_state, module)?;
+            }
+        }
     }
 
     Ok(())
@@ -33,5 +42,5 @@ pub fn build_script(out_dir: Option<&Path>) -> anyhow::Result<()> {
     let cargo_out_dir = std::env::var("OUT_DIR")?;
     let out_dir = out_dir.unwrap_or(Path::new(&cargo_out_dir));
 
-    build(Path::new("types"), out_dir)
+    build(Path::new("types"), out_dir, Backend::Rust)
 }
